e1f730e11dd97e280727796a41360ae26442c512
Paul Jacobs <paul.f.jacobs@gmail.com>
2017-02-25 21:33:58 -0500

    Test script. Tests for the feature gene. Initial operator gene.

--- mep/genetics/gene.py
+++ mep/genetics/gene.py
@@ -25,6 +25,7 @@ class Gene(object):
         :type constants: list
         :return: nothing; modifies the eval_matrix
         """
+# TODO: Should we also add a mutate method to the gene itself?
 
 
 class VariableGene(object):
@@ -53,7 +54,8 @@ class VariableGene(object):
         number of examples in the training data. The valuate at eval_matrix[i, j] then is the value of gene "i"
         evaluated with the data example (i.e. feature vector) at row "j" in the data matrix.
         :type eval_matrix: np.matrix
-        :param data_matrix: the data matrix; rows are feature vectors; comes from the data set
+        :param data_matrix: the data matrix; rows are feature vectors; comes from the data set; it is (n, m) where "n"
+        is the number of examples and "m" is the number of features.
         :type data_matrix: np.matrix
         :param constants: the constants associated with this chromosome
         :type constants: list
@@ -69,3 +71,50 @@ class VariableGene(object):
                 eval_matrix[gene_index, example_index] = data_matrix[example_index, self.index]
             else:
                 eval_matrix[gene_index, example_index] = constants[self.index]
+
+
+class OperatorGene(object):
+    """
+    This gene performance an operation on two addresses. The addresses are indices in the eval_matrix -- i.e. from the
+    evaluation of other genes before this one.
+    """
+
+    # NOTE: This could be expanded to multiple addresses
+    def __init__(self, operation, address1, address2):
+        """
+        Initialize.
+        :param operation: a lambda or function that can be operated on two floats
+        :type operation: lambda
+        :param address1: index into the eval_matrix
+        :type address1: int
+        :param address2: index into the eval_matrix
+        :type address2: int
+        """
+        self.operation = operation
+        self.address1 = address1
+        self.address2 = address2
+
+    def evaluate(self, gene_index, eval_matrix, data_matrix, constants):
+        """
+        This method will modify the eval_matrix for this gene index for each example in the data_matrix.
+
+        :param gene_index: the row index for this gene in the eval_matrix.
+        :type gene_index: int
+        :param eval_matrix: this is a (c, n) matrix where 'c' is the number of genes in the chromosome and 'n' is the
+        number of examples in the training data. The valuate at eval_matrix[i, j] then is the value of gene "i"
+        evaluated with the data example (i.e. feature vector) at row "j" in the data matrix.
+        :type eval_matrix: np.matrix
+        :param data_matrix: the data matrix; rows are feature vectors; comes from the data set
+        :type data_matrix: np.matrix
+        :param constants: the constants associated with this chromosome
+        :type constants: list
+        :return: nothing; modifies the eval_matrix
+        """
+        # go through and set the data
+        num_examples = eval_matrix.shape[1]
+        for example_index in range(0, num_examples):
+            # each column is one example in the data matrix (i.e. one feature vector)
+
+            # TODO: Catch errors; in particular division can be a problem
+            eval_matrix[gene_index, example_index] = self.operation(eval_matrix[self.address1][example_index],
+                                                                    eval_matrix[self.address2][example_index])
--- tests/mep/genetics/test_gene.py
+++ tests/mep/genetics/test_gene.py
@@ -34,3 +34,66 @@ class TestVariableGene(unittest.TestCase):
         gene.evaluate(gene_index, eval_matrix, data_matrix, constants)
         self.assertTrue(np.array_equal(expected_eval_matrix, eval_matrix))
 
+    def test_basic_feature_gene(self):
+        """
+        Simple check of a feature/input gene with just 1 gene in the chromosome.
+        """
+        # construct
+        feature_index = 1
+        gene = VariableGene(feature_index, is_feature=True)
+
+        # simple eval matrix; 1 gene in a chromosome, 3 examples, 2 constants
+        num_examples = 2
+        num_genes = 1
+        num_features = 3
+
+        # create
+        constants = [1., 2.]
+        eval_matrix = np.zeros((num_genes, num_examples))
+        data_matrix = np.zeros((num_examples, num_features))
+
+        # set the data matrix for the feature that we care about
+        data_matrix[0, feature_index] = 5.
+        data_matrix[1, feature_index] = 7.
+
+        # expected; only one gene and it is going to be using the first constant;
+        gene_index = 0
+        expected_eval_matrix = np.matrix([[data_matrix[0, feature_index], data_matrix[1, feature_index]]])
+
+        # run the evaluate
+        gene.evaluate(gene_index, eval_matrix, data_matrix, constants)
+        self.assertTrue(np.array_equal(expected_eval_matrix, eval_matrix))
+
+    def test_constant_and_feature_gene(self):
+        """
+        Intermix constant and feature genes.
+        """
+        # construct
+        feature_index = 1
+        constant_index = 0
+        constant_gene = VariableGene(constant_index, is_feature=False)
+        feature_gene = VariableGene(feature_index, is_feature=True)
+
+        # simple eval matrix; 1 gene in a chromosome, 3 examples, 2 constants
+        num_examples = 2
+        num_genes = 2
+        num_features = 3
+
+        # create
+        constants = [1., 2.]
+        eval_matrix = np.zeros((num_genes, num_examples))
+        data_matrix = np.zeros((num_examples, num_features))
+
+        # set the data matrix for the feature that we care about
+        data_matrix[0, feature_index] = 5.
+        data_matrix[1, feature_index] = 7.
+
+        # expected;
+        expected_eval_matrix = np.matrix([[data_matrix[0, feature_index], data_matrix[1, feature_index]],
+                                          [constants[constant_index], constants[constant_index]]])
+
+        # run the evaluate
+        feature_gene.evaluate(0, eval_matrix, data_matrix, constants)
+        constant_gene.evaluate(1, eval_matrix, data_matrix, constants)
+        self.assertTrue(np.array_equal(expected_eval_matrix, eval_matrix))
+
