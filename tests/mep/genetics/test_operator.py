27023d02c187b35d541652d77092e361eb1f2701
Paul Jacobs <paul.f.jacobs@gmail.com>
2018-01-13 15:08:17 -0500

    Test the other operators.

--- tests/mep/genetics/test_operator.py
+++ tests/mep/genetics/test_operator.py
@@ -1,5 +1,5 @@
 import unittest
-from mep.genetics.operator import MultiplicationOperator
+from mep.genetics.operator import MultiplicationOperator, AdditionOperator, SubtractionOperator
 
 
 class TestOperators(unittest.TestCase):
@@ -10,6 +10,35 @@ class TestOperators(unittest.TestCase):
     def test_multiplication_operator(self):
         """
         """
-        # construct the oeprator
+        # construct the operator and test it
         operator = MultiplicationOperator()
         self.assertEquals(5 * 2, operator(5, 2))
+        self.assertEquals("multiplication", operator.function_name())
+        self.assertEquals("""
+def multiplication(x, y):
+    return x * y
+        """, operator.function_python_definition())
+
+    def test_addition_operator(self):
+            """
+            """
+            # construct the operator and test it
+            operator = AdditionOperator()
+            self.assertEquals(5 + 2, operator(5, 2))
+            self.assertEquals("add", operator.function_name())
+            self.assertEquals("""
+def add(x, y):
+    return x + y
+        """, operator.function_python_definition())
+
+    def test_subtraction_operator(self):
+                """
+                """
+                # construct the operator and test it
+                operator = SubtractionOperator()
+                self.assertEquals(5 - 2, operator(5, 2))
+                self.assertEquals("subtraction", operator.function_name())
+                self.assertEquals("""
+def subtraction(x, y):
+    return x - y
+        """, operator.function_python_definition())
