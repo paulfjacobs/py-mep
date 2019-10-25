9f6a424108345834c528cc44356a7f10abbdd3dd
pjacobs <paul@kbit.com>
2019-10-25 16:42:32 +0000

    use arg parse

--- mep/main.py
+++ mep/main.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 import datetime as dt
 import json
@@ -7,17 +8,15 @@ from mep.dataset import DataSet
 from mep.model import MEPModel
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("ERROR: Expected usage 'python -m mep.main DATA_SET_NAME PYTHON_FILE_NAME'\n" +
-              "     DATA_SET_NAME:    The name (full path) to the data file to train on.\n"
-              "     PYTHON_FILE_NAME: The name (full path) to the python file to write the output program.\n"
-              "Example: 'python -m mep.main datasets/data1.csv test.py'"
-              )
-        sys.exit(-1)
+    parser = argparse.ArgumentParser(
+        description="Run the MEP model.\nExample: 'python -m mep.main datasets/data1.csv test.py", allow_abbrev=False)
+    parser.add_argument("data_set_name", help="The name (full path) to the data file to train on.")
+    parser.add_argument("python_file_name", help="The name (full path) to the python file to write the output program.")
+    args = parser.parse_args()
 
     # get the data file
-    data_set_name = sys.argv[1]
-    python_file_name = sys.argv[2]
+    data_set_name = args.data_set_name
+    python_file_name = args.python_file_name
     data_set = DataSet(data_set_name)
 
     # read config file
@@ -46,13 +45,13 @@ if __name__ == "__main__":
     model.fit(data_set.data_matrix, data_set.target)
     logger.info("Finished fitting the model")
 
-    # TODO: Optional?
     # we then convert the chromosome into a valid python program and write it out to file
-    with open(python_file_name, 'w') as python_file:
+    if python_file_name:
         python_program = model.to_python()
-        logger.debug("Write out the python program to {}".format(python_file_name))
-        logger.debug(python_program)
-        python_file.write(python_program)
+        with open(python_file_name, 'w') as python_file:
+            logger.debug("Write out the python program to {}".format(python_file_name))
+            logger.debug(python_program)
+            python_file.write(python_program)
 
     # TODO: Add support for classification
     # TODO: Add example digital circuit test
